ff8c5b4e02213227df353a88bb1dc0c5b72dff18
ruzell <arturocrisanto1@gmail.com>
2025-01-05 21:29:43 +0800

    Implement user management API with GET, POST, PATCH, and DELETE endpoints; add note management API for user-specific notes

--- app/api/(auth)/users/route.js
+++ app/api/(auth)/users/route.js
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import connect from "@/lib/db";
 import User from "@/modals/user";
+import { Types } from "mongoose";
 
 export const GET = async () => {
   try {
@@ -15,6 +16,12 @@ export const GET = async () => {
 export const POST = async (request) => {
   try {
     const body = await request.json();
+    if (!body.email || !body.username || !body.password) {
+      return new NextResponse(
+        JSON.stringify({ message: "Please Provide all the fields" }),
+        { status: 400 }
+      );
+    }
     await connect();
     const newUser = new User(body);
     await newUser.save();
@@ -25,6 +32,88 @@ export const POST = async (request) => {
   } catch (error) {
     return new NextResponse(
       { message: "Error in Creating Users", error },
+      { status: 500 }
+    );
+  }
+};
+// fetching the user by ID by using the body of the request
+export const PATCH = async (request) => {
+  try {
+    const body = await request.json();
+    const { userId, newUsername } = body;
+    await connect();
+
+    if (!userId || !newUsername) {
+      return new NextResponse(
+        JSON.stringify({ message: "Please Provide all the fields" }),
+        { status: 400 }
+      );
+    }
+    if (!Types.ObjectId.isValid(userId)) {
+      return new NextResponse(JSON.stringify({ message: "Invalid User ID" }), {
+        status: 400,
+      });
+    }
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      { username: newUsername },
+      { new: true }
+    );
+    if (!updatedUser) {
+      return new NextResponse(JSON.stringify({ message: "User Not Found" }), {
+        status: 404,
+      });
+    }
+    return new NextResponse(
+      JSON.stringify({ message: "User Updated", user: updatedUser }),
+      { status: 200 }
+    );
+  } catch (error) {
+    return new NextResponse(
+      JSON.stringify({ message: "Error in Updating Users", error }),
+      { status: 500 }
+    );
+  }
+};
+
+// fetching the user by ID by using the URL of the request
+export const DELETE = async (request) => {
+  try {
+    // Extracting the userId from the URL
+    const { searchParams } = new URL(request.url);
+    const userId = searchParams.get("userId");
+    // Check if the userId is provided
+    if (!userId) {
+      return new NextResponse(
+        JSON.stringify({ message: "Please Provide User ID" }),
+        { status: 400 }
+      );
+    }
+    // Check if the provided ID is a valid ObjectId
+    if (!Types.ObjectId.isValid(userId)) {
+      return new NextResponse(JSON.stringify({ message: "Invalid User ID" }), {
+        status: 400,
+      });
+    }
+    // Connect to the database
+    await connect();
+
+    // Find the user by ID and delete it
+    const deletedUser = await User.findByIdAndDelete(userId);
+    // If the user is not found, return 404
+    if (!deletedUser) {
+      return new NextResponse(JSON.stringify({ message: "User Not Found" }), {
+        status: 404,
+      });
+    }
+    // Return the deleted user
+    return new NextResponse(
+      JSON.stringify({ message: "User Deleted", user: deletedUser }),
+      { status: 200 }
+    );
+  } catch (error) {
+    return new NextResponse(
+      JSON.stringify({ message: "Error in Deleting Users", error }),
       { status: 500 }
     );
   }
---
+++ app/api/(dashboard)/notes/route.js
@@ -0,0 +1,77 @@
+import { NextResponse } from "next/server";
+import connect from "@/lib/db";
+import Note from "@/modals/notes";
+import { Types } from "mongoose";
+import User from "@/modals/user";
+
+// fetch notes of a specific user by userId
+export const GET = async (request) => {
+  try {
+    const { searchParams } = new URL(request.url);
+    const userId = searchParams.get("userId");
+
+    if (!userId || !Types.ObjectId.isValid(userId)) {
+      return new NextResponse(JSON.stringify({ message: "Invalid User ID" }), {
+        status: 400,
+      });
+    }
+
+    await connect();
+    const user = await User.findById(userId);
+    if (!user) {
+      return new NextResponse(JSON.stringify({ message: "User Not Found" }), {
+        status: 404,
+      });
+    }
+
+    const notes = await Note.find({ user: userId });
+    return new NextResponse(JSON.stringify(notes), { status: 200 });
+  } catch (error) {
+    return new NextResponse(
+      JSON.stringify({ message: "Error Fetching Notes", error }),
+      { status: 500 }
+    );
+  }
+};
+// can handle POST request to create notes while getting userId from query params
+export const POST = async (request) => {
+  try {
+    const { searchParams } = new URL(request.url);
+    const userId = searchParams.get("userId");
+    const { title, description } = await request.json();
+
+    // check if userId is valid
+    if (!userId || !Types.ObjectId.isValid(userId)) {
+      return new NextResponse(JSON.stringify({ message: "Invalid User ID" }), {
+        status: 400,
+      });
+    }
+
+    await connect();
+    // check if user exists
+    const user = await User.findById(userId);
+    if (!user) {
+      return new NextResponse(JSON.stringify({ message: "User Not Found" }), {
+        status: 404,
+      });
+    }
+    if (!title) {
+      return new NextResponse(
+        JSON.stringify({ message: "Title is Required" }),
+        { status: 400 }
+      );
+    }
+    const newNote = new Note({
+      title,
+      description,
+      user: userId,
+    });
+    await newNote.save();
+    return new NextResponse(JSON.stringify(newNote), { status: 201 });
+  } catch (error) {
+    return new NextResponse(
+      JSON.stringify({ message: "Error Creating Notes", error }),
+      { status: 500 }
+    );
+  }
+};
